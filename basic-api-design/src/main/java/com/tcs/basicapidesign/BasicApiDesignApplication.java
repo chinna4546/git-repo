d26542df20ab852ffbc342d406aa9e9752188177
chinna4546 <manikantakoribilli@gmail.com>
2022-03-18 21:40:14 +0530

    First API Project to git

---
+++ basic-api-design/src/main/java/com/tcs/basicapidesign/BasicApiDesignApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.basicapidesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApiDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicApiDesignApplication.class, args);
+	}
+
+}
---
+++ basic-api-design/src/test/java/com/tcs/basicapidesign/BasicApiDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.basicapidesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApiDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
